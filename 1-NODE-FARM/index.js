35ebf324fb570f2c8036134ec17c9b35c0aba3ce
AlexandrTimo <mr.atimoshenko@gmail.com>
2020-11-03 11:00:07 -0800

    Added Module 'http' and url routes(include json file)

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -1,7 +1,10 @@
 const { time } = require('console');
 const fs = require ('fs');
+const http = require ('http');
+const url = require ('url');
 
-
+/////////////////////////////////////////////////////////////////////////
+// FILE
 
 // Blocking / Synchronous way 
 
@@ -12,13 +15,11 @@ fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written!'); */
 
 
-
-
 // Non-Blocking / Asynchronous way
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+/* fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
     if (err) return console.log('ERROR :(((((');
-    
+
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         console.log(data2);
         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
@@ -30,4 +31,50 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
         });
     });
 });
-console.log('Code in proccessing...');
+console.log('Code in proccessing...'); */
+
+
+
+/////////////////////////////////////////////////////////////////////////
+// SERVER
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); // <-- storage data as it is (text format)
+const dataObj = JSON.parse(data); // <-- Create object for 'data.json' (convert file for use in code)
+
+const server = http.createServer((req, res) => { // Creating server, via Module 'http' and method 'createServer'
+    const pathName = req.url; // <-- request, can be multiple
+    const ex = `This is current URL: ${pathName}`;
+
+    if (pathName === '/' || pathName === '/overview'){ // <-- request, '/' or '/overview' url
+        res.end('Hello, My name is OVERVIEW!'); // <-- response (res.end), should be only one in block scope
+    }
+    else if (pathName === '/product'){ // <-- request, '/product' url
+        res.writeHead(200, {
+            'Content-style': 'text/html',
+            'Current-url': pathName
+        });
+        res.end(`<h1>hoho, Im, Mr.PRODUCT</h1> \n <h2>${ex}</h2>`);
+    }
+
+    else if (pathName === '/api'){ // <-- request, '/api' url
+            res.writeHead(200, { 
+                'Content-style': 'application/json',
+                'Current-url': pathName
+            });
+            res.end(data);
+    }
+
+    else {
+        res.writeHead(404, {
+            'Content-style': 'text/html',
+            'Current-url': pathName,
+            'My-own-header': 'Headerchjik'
+        });
+        res.end(`<h1>Page NOT found!</h1> \n <h2>${ex}</h2>`);
+    };
+});
+
+server.listen(5000, '127.0.0.1', () => {
+    console.log('Server listening...');
+})
