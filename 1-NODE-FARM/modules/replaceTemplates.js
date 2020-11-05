f49a405dabc613ff50105198e568d313fff3882e
AlexandrTimo <mr.atimoshenko@gmail.com>
2020-11-05 11:09:45 -0800

    Added logic in index.js, created templates and module/function

---
+++ 1-NODE-FARM/modules/replaceTemplates.js
@@ -0,0 +1,17 @@
+// Function for replace templates (Ex.{%PRICE%})
+
+module.exports = (temp, product) => {  // <-- temp - 'html file' / product - 'json list'
+  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
